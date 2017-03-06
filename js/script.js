d2df5fa4ba5e5286b19d7f8dc67e6a4a7b40e326
akonenkamp <akonenkamp@gmail.com>
2017-03-05 16:27:54 -0800

    adding sort function

--- js/script.js
+++ js/script.js
@@ -98,27 +98,77 @@ for(var i = 0; i<products.length; i++){
   console.log(products[i].description);
   console.log(products[i].price);
 }
- 
+
+function comparePrice(a, b) {
+  if (a.price < b.price) {
+    return -1;
+  }
+  if (a.price > b.price) {
+    return 1;
+  }
+  return 0;
+}
+
+function compareName(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+}
+
 function filterProducts(){
- console.log(document.sortBy.filter.value);
+  var sort_type = document.sortBy.filter.value;
+  if (sort_type === "price") {
+    console.log("sort by price");
+    products.sort(comparePrice);
+  } else if (sort_type === "name") {
+    console.log("sort by name");
+    products.sort(compareName);
+  }
+  console.log(products);
  event.preventDefault();
 }
 
 var cart = [];
 
+
+//function removeCart(name) {
+//  console.log(cart.length);
+//  console.log(name);
+//  console.log(cart);
+//}
+
+
 function addCart(name) {
+  var name_is_already_in_cart = false;
+  for (var i = 0; i < cart.length; i++) {
+    if (name === cart[i]) {
+      name_is_already_in_cart = true;
+    }
+  }
+  if (!name_is_already_in_cart) {
+      cart.push(name);
+  }
   console.log(cart.length);
   console.log(name);
+  console.log(cart);
 }
+
+
 
 function removeCart(name) {
+
+  for (var i = 0; i < cart.length; i++) {
+    if (name === cart[i]) {
+      cart.splice(i, 1);
+    
+    }
+  }
+  
   console.log(cart.length);
   console.log(name);
+  console.log(cart);
 }
-
-
-
-
-
-
-
